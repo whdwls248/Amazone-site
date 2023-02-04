99dedb94f555b2630c1758dc8cd81bfa8877a0f2
Won JongJin <dmlrud1005@naver.com>
2023-02-04 18:47:53 +0900

    nav bar 초기 작업

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCHk1EWwHkdTYkso5mfYW1JA1yjF5qxozc",
+  authDomain: "fir-b5b2c.firebaseapp.com",
+  projectId: "fir-b5b2c",
+  storageBucket: "fir-b5b2c.appspot.com",
+  messagingSenderId: "819128498366",
+  appId: "1:819128498366:web:2a08945597327325fd86ef",
+  measurementId: "G-9LH1PCF69T",
+};
