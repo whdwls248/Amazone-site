896ee042d1b1e7c12ab7854f5adde70d6c89ec0f
Won JongJin <dmlrud1005@naver.com>
2023-03-02 17:36:50 +0900

    - Payment processing and connection with firestore

--- src/Payment.js
+++ src/Payment.js
@@ -7,6 +7,7 @@ import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./Reducer";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import axios from "./axios";
+import { db } from "./firebase";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -33,6 +34,16 @@ function Payment() {
         },
       })
       .then(({ paymentIntent }) => {
+        db.collection("users")
+          .doc(user?.uid)
+          .collection("orders")
+          .doc(paymentIntent.id)
+          .set({
+            basket: basket,
+            amount: paymentIntent.amount,
+            created: paymentIntent.created,
+          });
+
         setSucceeded(true);
         setError(null);
         setProcessing(false);
